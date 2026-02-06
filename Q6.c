395452c2d2182447458fa69efbe78495ef52c22c
AyushSingh-022007 <suir8780@gmail.com>
2026-02-06 22:41:54 +0530

    Day 6

---
+++ Q6.c
@@ -0,0 +1,26 @@
+// Problem: Given a sorted array of n integers, remove duplicates in-place. Print only unique elements in order.
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements (sorted):\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    if (n > 0)
+    {
+        printf("Unique elements: %d ", arr[0]);
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] != arr[i - 1])
+            {
+                printf("%d ", arr[i]);
+            }
+        }
+    }
+    return 0;
+}
