e1590fa694ab33c13cfad730e769d8d08263e310
AyushSingh-022007 <suir8780@gmail.com>
2026-02-16 14:14:26 +0530

    Day 16

---
+++ Q16.c
@@ -0,0 +1,33 @@
+// Problem: Given an array of integers, count the frequency of each distinct element and print the result.
+#include <stdio.h>
+int main()
+{
+    int n, i, j, count;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    int visited[n];
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        visited[i] = 0;
+    }
+    printf("\nElement   Frequency\n");
+    for (i = 0; i < n; i++)
+    {
+        if (visited[i] == 1)
+            continue;
+        count = 1;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+                visited[j] = 1;
+            }
+        }
+        printf("%d \t   %d\n", arr[i], count);
+    }
+    return 0;
+}
